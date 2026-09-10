0f298c95df88a043f18f9569c54f76a8243735d2
manuel Fernandez <manuelfernandezb24@gmail.com>
2026-09-10 08:31:22 -0500

    Fix class name typo for cuentaDeAhorro

--- cooperativaEstudiantil-OOP-main/main.java
+++ cooperativaEstudiantil-OOP-main/main.java
@@ -1,6 +1,6 @@
 public class main{
 
     public static void main(String[] args){
-        cuentaDeAhorro cliente = new cuentaDAhorro("laura piedrahita","91293572033");
+        cuentaDeAhorro cliente = new cuentaDeAhorro("laura piedrahita","91293572033");
     }
 }
